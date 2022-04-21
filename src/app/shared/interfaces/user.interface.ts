2a3fcdf25c67785322091625a76a6d94128b5f69
William Talley <wtalley@my365.bellevue.edu>
2022-04-21 17:29:10 -0500

    sprint1 push

---
+++ src/app/shared/interfaces/user.interface.ts
@@ -0,0 +1,21 @@
+/*
+============================================
+; Title: WEB450 Bob's Computer Repair SHop
+; Author: Professor Krasso
+; Date: April 21, 2022
+; Modified By: House Gryffindor
+; Description: Bob's Computer Repair Shop App security-questions.interface file
+; interface for user information
+;===========================================
+*/
+
+export interface User {
+    _id?: string;
+    userName?: string;
+    password?: string;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    address: string;
+    email: string;
+}
