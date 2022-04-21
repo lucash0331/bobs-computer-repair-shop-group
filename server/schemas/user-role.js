96e53d1ce4722622ff0874b60bf17cffdaf96747
Lucas Hoffman <75821031+lucash0331@users.noreply.github.com>
2022-04-21 16:21:32 -0400

    Adding user-role.js & user.js

--- server/schemas/user-role.js
+++ server/schemas/user-role.js
@@ -8,3 +8,12 @@
 ; Schema for user roles
 ;===========================================
 */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userRoleSchema = new Schema({
+  role: { type: String, default: 'standard' }
+})
+
+module.exports = userRoleSchema
