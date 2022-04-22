50137ba50e3631b85ee0e5a9a939370dc47a56b8
Adam School <adrodgers@my365.bellevue.edu>
2022-04-22 12:29:17 -0500

    API Updates

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use("/", express.static(path.join(__dirname, "../dist/bcrs")));
 /**
  * Variables
  */
-const port = 3000; // server port
+const port = process.env.PORT || 3000; // server port
 
 // TODO: This line will need to be replaced with your actual database connection string
 const conn = "mongodb+srv://bcrs:bcrsweb450@buwebdev-cluster-1.zjoha.mongodb.net/bcrs?retryWrites=true&w=majority";
