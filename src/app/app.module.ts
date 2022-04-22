a4b246fc70f0b9e30dcd3b7fae5088b2301134f2
Adam School <adrodgers@my365.bellevue.edu>
2022-04-22 18:05:47 -0500

    Updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './pages/home/home.component';
-import { BaseLayoutComponent } from './shared/base-layout/base-layout.component';
-import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HomeComponent } from "./pages/home/home.component";
+import { BaseLayoutComponent } from "./shared/base-layout/base-layout.component";
+import { AuthLayoutComponent } from "./shared/auth-layout/auth-layout.component";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FlexModule } from "@angular/flex-layout";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatDividerModule } from "@angular/material/divider";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    BaseLayoutComponent,
-    AuthLayoutComponent
-  ],
+  declarations: [AppComponent, HomeComponent, BaseLayoutComponent, AuthLayoutComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -32,8 +34,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    FlexModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatDividerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/shared/base-layout/base-layout.component.ts
+++ src/app/shared/base-layout/base-layout.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { CookieService } from "ngx-cookie-service";
 
 @Component({
-  selector: 'app-base-layout',
-  templateUrl: './base-layout.component.html',
-  styleUrls: ['./base-layout.component.css']
+  selector: "app-base-layout",
+  templateUrl: "./base-layout.component.html",
+  styleUrls: ["./base-layout.component.css"],
 })
 export class BaseLayoutComponent implements OnInit {
-
   year: number = Date.now();
-  
-  constructor() { }
 
-  ngOnInit(): void {
+  isLoggedIn: boolean;
+  name: string;
+
+  constructor(private cookieService: CookieService, private router: Router) {
+    this.isLoggedIn = this.cookieService.get("session_user") ? true : false;
   }
 
+  ngOnInit(): void {
+    this.name = sessionStorage.getItem("name");
+  }
+
+  signOut() {
+    this.cookieService.deleteAll();
+    this.router.navigate(["/session/signin"]);
+  }
 }
