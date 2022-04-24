26be4755a6a077caf224044df7999ec82cb874c3
Lucas Hoffman <75821031+lucash0331@users.noreply.github.com>
2022-04-24 14:31:34 -0400

    Adding ts file to create security question

--- src/app/pages/security-question-create/security-question-create.component.ts
+++ src/app/pages/security-question-create/security-question-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SecurityQuestionsService } from '../../services/security-questions.service'
+import { SecurityQuestion } from '../../shared/interfaces/security-questions.interface';
+
 
 @Component({
   selector: 'app-security-question-create',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecurityQuestionCreateComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private securityQuestionsService: SecurityQuestionsService
+  ) {}
+
+  ngOnInit() {
+    // This is for validators.
+    this.form = this.fb.group({
+      text: [null, Validators.compose([Validators.required])],
+    });
   }
 
+  // This is the create function
+  create() {
+    const newSecurityQuestion = {} as SecurityQuestion;
+    newSecurityQuestion.text = this.form.controls.text.value;
+    // This function will be correct one the createSecurity API is created in the security-question.service
+    this.securityQuestionsService
+      .createSecurityQuestion(newSecurityQuestion)
+      .subscribe(
+        (res) => {
+          this.router.navigate(['/security-questions']);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
+
+  // This is the cancel button.
+  cancel() {
+    this.router.navigate(['/security-questions']);
+  }
 }
