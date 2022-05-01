70e046112a570df54a407e76f494f4bfeec4e91b
William Talley <wtalley@my365.bellevue.edu>
2022-05-01 14:22:36 -0500

    sprint2 push; 404/500 changes

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -4,24 +4,19 @@
 ; Author: Professor Krasso
 ; Date: April 29, 2022
 ; Modified By: House Gryffindor
-; Description: Bob's Computer Repair Shop App ts code for when a 4040 error occurs 
+; Description: Bob's Computer Repair Shop App ts code for when a 404 error occurs 
 ===========================================
 */
 
-
-
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  selector: "app-not-found",
+  templateUrl: "./not-found.component.html",
+  styleUrls: ["./not-found.component.css"],
 })
 export class NotFoundComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
