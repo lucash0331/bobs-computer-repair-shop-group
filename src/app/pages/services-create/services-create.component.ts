acdfb4a1cdc0e56bd177fc0a80dafa521bc8a67f
Adam School <adrodgers@my365.bellevue.edu>
2022-05-07 11:28:02 -0500

    Services API update

--- src/app/pages/services-create/services-create.component.ts
+++ src/app/pages/services-create/services-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Service } from 'src/app/shared/interfaces/services.interface';
+import { ServicesService } from 'src/app/services/services.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-services-create',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services-create.component.css']
 })
 export class ServicesCreateComponent implements OnInit {
+  form: FormGroup
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private servicesService: ServicesService
+    ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): {
+    this.form = this.fb.form({
+      text: [null, Validators.compose([Validators.required])]
+    })
   }
 
+  create() {
+    const newService = {} as Service;
+    newService.name = this.form.controls.name.value
+    this.servicesService.createService(newService).subscribe(
+      (res) => {
+        this.router.navigate(["/services"])
+      },
+      (err) => {
+        console.log(err)
+      }
+    )
+  }
 }
