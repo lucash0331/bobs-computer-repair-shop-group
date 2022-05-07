55d0baa372debde2bad1a37a52d6110d1c5ecad1
Adam School <adrodgers@my365.bellevue.edu>
2022-05-07 11:32:28 -0500

    Services API update

--- src/app/pages/services-create/services-create.component.ts
+++ src/app/pages/services-create/services-create.component.ts
@@ -1,39 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Service } from 'src/app/shared/interfaces/services.interface';
-import { ServicesService } from 'src/app/services/services.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Service } from "src/app/shared/interfaces/services.interface";
+import { ServicesService } from "src/app/services/services.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-services-create',
-  templateUrl: './services-create.component.html',
-  styleUrls: ['./services-create.component.css']
+  selector: "app-services-create",
+  templateUrl: "./services-create.component.html",
+  styleUrls: ["./services-create.component.css"],
 })
 export class ServicesCreateComponent implements OnInit {
-  form: FormGroup
+  form: FormGroup;
 
-  constructor(
-    private fb: FormBuilder,
-    private router: Router,
-    private servicesService: ServicesService
-    ) {}
+  constructor(private fb: FormBuilder, private router: Router, private servicesService: ServicesService) {}
 
-  ngOnInit(): {
-    this.form = this.fb.form({
-      text: [null, Validators.compose([Validators.required])]
-    })
+  ngOnInit() {
+    this.form = this.fb.group({
+      text: [null, Validators.compose([Validators.required])],
+    });
   }
 
   createService() {
     const newService = {} as Service;
-    newService.name = this.form.controls.name.value
+    newService.name = this.form.controls.name.value;
     this.servicesService.createService(newService).subscribe(
       (res) => {
-        this.router.navigate(["/services"])
+        this.router.navigate(["/services"]);
       },
       (err) => {
-        console.log(err)
+        console.log(err);
       }
-    )
+    );
   }
 }
