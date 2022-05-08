cc380b3a1153dbc204c1b090564d3fedc8e1a86a
William Talley <wtalley@my365.bellevue.edu>
2022-05-07 20:00:02 -0500

    Sprint3 push

--- server/models/role.js
+++ server/models/role.js
@@ -12,12 +12,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const roleSchema = new Schema(
-  {
-    text: { type: String, unique: true },
-    isDisabled: { type: Boolean, default: false },
-  },
-  { collection: "roles" }
-);
+const roleSchema = new Schema({
+  text: { type: String, unique: true },
+  isDisabled: { type: Boolean, default: false },
+});
 
 module.exports = mongoose.model("Role", roleSchema);
---
+++ src/app/pages/purchases-by-service-graph/purchases-by-service-graph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-purchases-by-service-graph',
+  templateUrl: './purchases-by-service-graph.component.html',
+  styleUrls: ['./purchases-by-service-graph.component.css']
+})
+export class PurchasesByServiceGraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
