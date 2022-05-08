02c0064806edee03fb77b0afab178384482fca18
William Talley <wtalley@my365.bellevue.edu>
2022-05-07 21:38:22 -0500

    Sprint3 push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -63,10 +63,10 @@ import { UserProfileComponent } from "./pages/user-profile/user-profile.componen
 import { ServicesListComponent } from "./pages/services-list/services-list.component";
 import { ServicesCreateComponent } from "./pages/services-create/services-create.component";
 import { ServicesDetailsComponent } from "./pages/services-details/services-details.component";
-import { RoleListComponent } from './pages/role-list/role-list.component';
-import { RoleCreateComponent } from './pages/role-create/role-create.component';
-import { RoleDetailsComponent } from './pages/role-details/role-details.component';
-import { PurchasesByServiceGraphComponent } from './pages/purchases-by-service-graph/purchases-by-service-graph.component';
+import { RoleListComponent } from "./pages/role-list/role-list.component";
+import { RoleCreateComponent } from "./pages/role-create/role-create.component";
+import { RoleDetailsComponent } from "./pages/role-details/role-details.component";
+
 //import { InvoiceDialogComponent } from './shared/invoice-dialog/invoice-dialog.component';
 
 @NgModule({
@@ -97,7 +97,6 @@ import { PurchasesByServiceGraphComponent } from './pages/purchases-by-service-g
     RoleListComponent,
     RoleCreateComponent,
     RoleDetailsComponent,
-    PurchasesByServiceGraphComponent,
     //InvoiceDialogComponent, commented out because the invoice component dialog not pushed to git
   ],
   imports: [
